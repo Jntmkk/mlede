d09a6c4818e84f80b9cdc68dc0848cb142832173
Jntmkk <bbmike1086@gmail.com>
2022-04-15 23:04:18 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/Jntmkk/OpenClash.git package/OpenClash
+git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+git clone https://github.com/hyrathb/mentohust.git package/mentohust
+git clone https://github.com/CHN-beta/xmurp-ua.git package/xmurp
