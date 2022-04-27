d7fb61b2b4fd4291d365465b5ad6370520b4a153
Jntmkk <55786908+Jntmkk@users.noreply.github.com>
2022-04-27 10:23:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
 git clone https://github.com/hyrathb/mentohust.git package/mentohust
 git clone https://github.com/CHN-beta/xmurp-ua.git --branch dev package/xmurp
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
